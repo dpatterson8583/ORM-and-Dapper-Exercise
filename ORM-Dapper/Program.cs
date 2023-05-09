0b42e9c0e49e82d772f4f3a2e06cb2ab72d9117f
dpatterson8583 <dpatterson8583@gmail.com>
2023-05-09 15:47:16 -0500

    Initial commit for ORM-Dapper

--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Data;
+using System;
+using System.IO;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var repo = new DapperDepartmentRepository(conn);
+
+           // Console.WriteLine("Type a new Department Name");
+
+           // var newDepartment = Console.ReadLine();
+
+           // repo.InsertDepartment(newDepartment);
+
+            var departments = repo.GetAllDepartments();
+
+            foreach (var dept in departments) 
+            {
+                Console.WriteLine($"Departments: {dept.Name}");
+            }
+
         }
     }
 }
